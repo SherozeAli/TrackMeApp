005646dbeaf2ac39d5daf8b04e5e651bc4fdd72d
SherozeAli <sherozeali777@gmail.com>
2020-03-09 21:06:44 +0500

    created track-schema and tested on postman

--- track-server/src/index.js
+++ track-server/src/index.js
@@ -1,14 +1,19 @@
 require('./models/User')
+require('./models/Track')
 
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
 const authRoutes = require('./routes/authRoutes')
+const trackRoutes = require('./routes/trackRoutes')
 const requireAuth = require('./middlewares/requireAuth')
+
 const app = express()
 
 app.use(bodyParser.json())
 app.use(authRoutes)
+app.use(trackRoutes)
+
 app.get('/', requireAuth, (req, res) => {
   res.send(`Your email: ${req.user.email} `)
 })
