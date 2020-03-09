de466b8f29b85fe4902e2005c3ceb98defc4ace6
SherozeAli <sherozeali777@gmail.com>
2020-03-09 18:00:56 +0500

    password hashing and signup signin completed

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -16,8 +16,23 @@ router.post('/signup', async (req, res) => {
   }
 })
 
-// router.get('/signin', (req, res) => {
-//   console.log(req.body)
-// })
+router.post('/signin', async (req, res) => {
+  const { email, password } = req.body
+  if (!email && !password) {
+    return res.status(422).send({ error: 'Must Provide email and password' })
+  }
+  const user = await User.findOne({ email })
+  if (!user) {
+    return res.status(422).send({ error: 'Email not found !' })
+  }
+
+  try {
+    await user.comparePassword(password)
+    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY')
+    res.send({ token })
+  } catch (err) {
+    return res.status(422).send({ error: 'INVALID PASS AND EMAIL!' })
+  }
+})
 
 module.exports = router
