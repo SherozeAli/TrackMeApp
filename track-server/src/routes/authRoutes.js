2d4b729877ecc57b58dbe1d6df19c7b97633e1b0
SherozeAli <sherozeali777@gmail.com>
2020-03-09 17:14:34 +0500

    JWT configured

--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const User = mongoose.model('User')
-
+const jwt = require('jsonwebtoken')
 const router = express.Router()
 router.post('/signup', async (req, res) => {
   const { email, password } = req.body
@@ -9,7 +9,8 @@ router.post('/signup', async (req, res) => {
     const user = new User({ email, password })
     await user.save()
 
-    res.send('YOU MADE A POST REQUEST ')
+    const token = jwt.sign({ userId: user._id }, 'MY_SECRET_KEY')
+    res.send({ token })
   } catch (err) {
     return res.status(422).send(err.message)
   }
